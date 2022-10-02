abb46d9a31704cf6dc282f3d6ad6895244c44f20
Niklas Dewally <niklas@dewally.com>
2022-10-02 22:33:53 +0100

    README

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-mvn clean 
-mvn package
+./mvnw clean 
+./mvnw package
 echo "$1" | java --enable-preview -jar target/parser-demo-1.0-SNAPSHOT.jar 
 bash stitch_gif.sh "$1"
 rm graph-*.png
